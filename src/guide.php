c95d34863d28fed10a18a895a0398e5ecacb3aea
Sumanathilaka <37451505+Sumanathilaka@users.noreply.github.com>
2018-03-22 11:59:14 +0530

    Update guide.php

--- src/guide.php
+++ src/guide.php
@@ -9,28 +9,11 @@ session_start();
 <link rel="icon" href="nitc.png">
 
 <style>
-    #records {
-    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-}
-#records td, #records th {
-    border: 1px solid #ddd;
-    padding: 8px;
-}
-#records tr:nth-child(even){background-color: #f2f2f2;}
-#records tr:hover {background-color: #ddd;}
-#records th {
-    padding-top: 12px;
-    padding-bottom: 12px;
-    text-align: left;
-    background-color: #ce4012;
-    color: white;
-}
-    
+
 body{
     background-color: #f2f2f2;
 }
+
 input[type=submit] {
     background-color: #4CAF50;
     color: white;
@@ -39,6 +22,7 @@ input[type=submit] {
     border-radius: 4px;
     cursor: pointer;
 }
+
 button{
     background-color: #4CAF90;
     color: white;
@@ -46,7 +30,9 @@ button{
     border: none;
     border-radius: 4px;
     cursor: pointer;
+
 }
+
 input[type=text], select, textarea{
     width: 90%; /* Full width */
     padding: 12px; /* Some padding */ 
@@ -57,6 +43,7 @@ input[type=text], select, textarea{
     margin-bottom: 16px; /* Bottom margin */
     resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
 }
+
 </style>
 </head>
 
@@ -70,6 +57,7 @@ input[type=text], select, textarea{
 <br><br>
 
 <?php
+
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $servername = $url["host"];
 $username = $url["user"];
@@ -77,54 +65,59 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
+
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 $roll="";
 $name=$_SESSION['username'];
+
 $sql = "SELECT name,project.rollno,email,department,guide,guidemail,topic,status,date
 FROM project,mtechstudent 
 where project.rollno=mtechstudent.rollno and guidemail='$name'";
 $result= mysqli_query($conn, $sql);
 $availability=0;
+
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
             $roll=$row['rollno'];
              $availability=1;
              
-      
-echo  "<table id = 'records'>"; 
-  echo "<tr>";
-      echo  "<th>Name</th>";
-      echo  "<th>Roll No</th>";
-      echo  "<th>Project</th>";
-      echo  "<th>Guided By</th>";
-      echo  "<th>Present Status</th>";
-      echo  "<th>Last Modified Date</th>";
-      echo  "<th>History</th>";
-  echo "</tr>";    
-if (mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
-              
-             $availability=1;
-              $roll=$row['rollno'];
-  ?>       
-            <?php
-        
-                echo "<tr><td>", $row['name'] , "</td><td>" , $row['rollno'] , "</td><td>" , $row['topic'] , "</td><td>" , $row['guide'] , "</td><td>", $row['status'] , "</td><td>" , $row['date'], "</td><td><form action = 'history.php' method = 'post'><input type = 'hidden' name = 'rollno' value = ", $roll, "><input type = 'submit' value = 'History'></form>", "</td></tr>" ;?>
 
+  ?>
+          <div style="width: 30%;float:left;padding: 15px;background-color: #e9dbd8;margin: 5px" >
+           
+            <?php
+  echo  "<h4>Name:  " . $row["name"]."<br> ". " Roll No :  " . $row["rollno"]. " <br> ". " Project : " . $row["topic"]. "<br> "." Guided By: " . $row["guide"]."<br>  ".  "Present Status : " . $row["status"]."<br>  ". "Modified Date : " . $row["date"]."<br>" ; 
+
+?>
+<br>
+
+<center>
+ <form action="historyuser.php" method="post" >
+  <input type="hidden" name="rollno" value=<?php echo $roll ?> >
+  <input type="submit" value="History">
+</form>
+</center>
+</div>
 
 <?php
 }
 }
+
+
+
 if($availability==0)
 {
+
   ?>
  <center>
   <?php
   echo "<br><br><br>";
    echo "No projects are available";
 }
+
                   
 mysqli_close($conn);
 ?> 
@@ -146,5 +139,10 @@ mysqli_close($conn);
   </center>
 </footer>
    
+</body>
+</html>
+
+
+
 </body>
 </html>
