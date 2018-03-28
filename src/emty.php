2b29f9764b4a677d5b04b66323059b371c6f5c4a
Sumanathilaka <37451505+Sumanathilaka@users.noreply.github.com>
2018-03-28 09:34:48 +0530

    Update emty.php

--- src/emty.php
+++ src/emty.php
@@ -9,10 +9,10 @@ $db = substr($url["path"], 1);
 $conn = new mysqli($servername, $username, $password, $db);
 
 $dbName = "deanproject";
-mysqli_select_db($dbName); /*added semi-colon*/
-$result_t = mysqli_query("SHOW TABLES");
-while($row = mysqli_fetch_assoc($result_t))
+mysqli_select_db($conn,$dbName); /*added semi-colon*/
+$result_t = mysqli_query($conn,"SHOW TABLES");
+while($row = mysqli_fetch_assoc($conn,$result_t))
 {
-   mysqli_query("TRUNCATE " . $row['Tables_in_' . $dbName]);
+   mysqli_query($conn,"TRUNCATE " . $row['Tables_in_' . $dbName]);
 }
 ?>
