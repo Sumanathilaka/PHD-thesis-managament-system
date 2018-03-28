d5495b39b7c8d55d0841793489f2e48f6ed2008b
Sumanathilaka <37451505+Sumanathilaka@users.noreply.github.com>
2018-03-28 09:48:02 +0530

    Update fulleditupdate.php

--- src/fulleditupdate.php
+++ src/fulleditupdate.php
@@ -29,7 +29,7 @@ if ($conn->connect_error) {
       $guide=mysqli_real_escape_string($conn, $_POST['guidance']);
       $guide_emailid=mysqli_real_escape_string($conn, $_POST['guide_emailid']);
       $topic=mysqli_real_escape_string($conn, $_POST['topic']);
-
+      $guide2=mysqli_real_escape_string($conn, $_POST['guide2']);
 	   $roll=mysqli_real_escape_string($conn, $_POST['roll']);
 	 
 
@@ -41,7 +41,7 @@ $sql7 = "UPDATE mtechstudent SET email='$email' WHERE rollno='$roll' ";
 $sql8 = "UPDATE mtechstudent SET department='$department' WHERE rollno='$roll' ";
 $sql9 = "UPDATE mtechstudent SET guide='$guide' WHERE rollno='$roll' ";
 $sql10 = "UPDATE mtechstudent SET guidemail='$guide_emailid' WHERE rollno='$roll' ";
-
+$sql19 = "UPDATE mtechstudent SET guide2='$guide2' WHERE rollno='$roll' ";
 $result= mysqli_query($conn, $sql);
 $result1= mysqli_query($conn, $sql2);
 $result5= mysqli_query($conn, $sql5);
@@ -49,6 +49,7 @@ $result6= mysqli_query($conn, $sql6);
 $result7= mysqli_query($conn, $sql7);
 $result8= mysqli_query($conn, $sql8);
 $result9= mysqli_query($conn, $sql9);
+$result9= mysqli_query($conn, $sql19);	
 
 
 
