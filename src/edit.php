4cf67acedd7b48893ef6173db17864145d671201
mbasilak <37478378+mbasilak@users.noreply.github.com>
2018-03-28 18:20:40 +0530

    Update edit.php

--- src/edit.php
+++ src/edit.php
@@ -12,14 +12,12 @@ if(!isset($_SESSION['username'])) {
 <link rel="icon" href="nitc.png">
 
 <style>
-
 body{
     background-color: #f2f2f2;
 }
-
 input[type=text], select, textarea{
     width: 100%; /* Full width */
-    padding: 12px; /* Some padding */ 
+    padding: 12px; /* Some padding */
     border: 1px solid #ccc; /* Gray border */
     border-radius: 4px; /* Rounded borders */
     box-sizing: border-box; /* Make sure that padding and width stays in place */
@@ -27,10 +25,9 @@ input[type=text], select, textarea{
     margin-bottom: 16px; /* Bottom margin */
     resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
 }
-
 input[type=date], select, textarea{
     width: 100%; /* Full width */
-    padding: 12px; /* Some padding */ 
+    padding: 12px; /* Some padding */
     border: 1px solid #ccc; /* Gray border */
     border-radius: 4px; /* Rounded borders */
     box-sizing: border-box; /* Make sure that padding and width stays in place */
@@ -38,10 +35,9 @@ input[type=date], select, textarea{
     margin-bottom: 16px; /* Bottom margin */
     resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
 }
-
 input[type=time], select, textarea{
     width: 100%; /* Full width */
-    padding: 12px; /* Some padding */ 
+    padding: 12px; /* Some padding */
     border: 1px solid #ccc; /* Gray border */
     border-radius: 4px; /* Rounded borders */
     box-sizing: border-box; /* Make sure that padding and width stays in place */
@@ -49,11 +45,9 @@ input[type=time], select, textarea{
     margin-bottom: 16px; /* Bottom margin */
     resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
 }
-
-
 input[type=file], select, textarea{
     width: 100%; /* Full width */
-    padding: 12px; /* Some padding */ 
+    padding: 12px; /* Some padding */
     border: 1px solid #ccc; /* Gray border */
     border-radius: 4px; /* Rounded borders */
     box-sizing: border-box; /* Make sure that padding and width stays in place */
@@ -61,7 +55,6 @@ input[type=file], select, textarea{
     margin-bottom: 16px; /* Bottom margin */
     resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
 }
-
 /* Style the submit button with a specific background color etc */
 input[type=submit] {
     background-color: #4CAF50;
@@ -71,19 +64,16 @@ input[type=submit] {
     border-radius: 4px;
     cursor: pointer;
 }
-
 /* When moving the mouse over the submit button, add a darker green color */
 input[type=submit]:hover {
     background-color: #45a049;
 }
-
 /* Add a background color and some padding around the form */
 .container {
     border-radius: 5px;
     background-color: #f2f2f2;
     padding: 20px;
 }
-
 </style>
 
 
@@ -99,7 +89,76 @@ input[type=submit]:hover {
 <br><br>
 
 <?php
-    
+$status1 = array
+  (
+    array("SynMailtoE1L1-1",14),
+    array("SynRem1toE1L1-1",7),
+    array("SynRem2toE1L1-1",7),
+    array("SynMailtoE1L1-2",14),
+    array("SynRem1toE1L1-2",7),
+    array("SynRem2toE1L1-2",7),
+    array("SynMailtoE1L1-3",14),
+    array("SynRem1toE1L1-3",7),
+    array("SynRem2toE1L1-3",7),
+    array("SynMailtoE1L1-4",14),
+    array("SynRem1toE1L1-4",7),
+    array("SynRem2toE1L1-4",7),
+    array("SynMailtoE1L2-1",14),
+    array("SynRem1toE1L2-1",7),
+    array("SynRem2toE1L2-1",7),
+    array("SynMailtoE1L2-2",14),
+    array("SynRem1toE1L2-2",7),
+    array("SynRem2toE1L2-2",7),
+    array("SynMailtoE1L2-3",14),
+    array("SynRem1toE1L2-3",7),
+    array("SynRem2toE1L2-3",7),
+    array("SynMailtoE1L2-4",14),
+    array("SynRem1toE1L2-4",7),
+    array("SynRem2toE1L2-4",7),
+    array("E1ReadyForThesisReview",NULL),
+    array("ThesisSentToE1",45),
+    array("ThesisE1Rem1",7),
+    array("ThesisE1Rem2",7),
+    array("ThesisE1Rem3",7),
+    array("ThesisE1Rem4",7),
+    array("E1ApprovedThesis",7),
+    array("E1ApprovedHonInitiated",NULL)
+   );
+   $status2 = array
+     (
+       array("SynMailtoE2L1-1",14),
+       array("SynRem1toE2L1-1",7),
+       array("SynRem2toE2L1-1",7),
+       array("SynMailtoE2L1-2",14),
+       array("SynRem1toE2L1-2",7),
+       array("SynRem2toE2L1-2",7),
+       array("SynMailtoE2L1-3",14),
+       array("SynRem1toE2L1-3",7),
+       array("SynRem2toE2L1-3",7),
+       array("SynMailtoE2L1-4",14),
+       array("SynRem1toE2L1-4",7),
+       array("SynRem2toE2L1-4",7),
+       array("SynMailtoE2L2-1",14),
+       array("SynRem1toE2L2-1",7),
+       array("SynRem2toE2L2-1",7),
+       array("SynMailtoE2L2-2",14),
+       array("SynRem1toE2L2-2",7),
+       array("SynRem2toE2L2-2",7),
+       array("SynMailtoE2L2-3",14),
+       array("SynRem1toE2L2-3",7),
+       array("SynRem2toE2L2-3",7),
+       array("SynMailtoE2L2-4",14),
+       array("SynRem1toE2L2-4",7),
+       array("SynRem2toE2L2-4",7),
+       array("E2ReadyForThesisReview",NULL),
+       array("ThesisSentToE2",45),
+       array("ThesisE2Rem1",7),
+       array("ThesisE2Rem2",7),
+       array("ThesisE2Rem3",7),
+       array("ThesisE2Rem4",7),
+       array("E2ApprovedThesis",7),
+       array("E2ApprovedHonInitiated",NULL)
+      );
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $servername = $url["host"];
 $username = $url["user"];
@@ -107,62 +166,70 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
-
-
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 $rollno=mysqli_real_escape_string($conn, $_POST['rollno']);
-
-$sql = "SELECT name,project.rollno,email,department,guide,guidemail,topic,status,date
-FROM project,mtechstudent 
+$sql = "SELECT name,project.rollno,email,department,guide,guidemail,topic,status1,date1,status2,date2
+FROM project,mtechstudent
 where project.rollno=mtechstudent.rollno
  and project.rollno='$rollno'";
 $result= mysqli_query($conn, $sql);
-
-
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
             $roll=$row['rollno'];
+            $statuse1=$row['status1'];
+            $statuse2=$row['status2'];
+            $date1=$row['date1'];
+            $date2=$row['date2'];
   ?>
 
 
           <div style="padding: 15px;background-color: #e9dbd8;" >
-           
+
     <?php
-  echo  "<h4>Name:  " . $row["name"]."<br> ". " Roll No :  " . $row["rollno"]. " <br> ". " Project : " . $row["topic"]. "<br> "." Guided By: " . $row["guide"]."<br>  ".  "Present Status : " . $row["status"]."<br>  ". "Modified Date : " . $row["date"]."<br>" ; 
-
+  echo  "<h4>Name:  " . $row["name"]."<br> ". " Roll No :  " . $row["rollno"]. " <br> ". " Project : " . $row["topic"]. "<br> "." Guided By: " . $row["guide"]."<br>  ".  "Present Status : " . $row["status"]."<br>  ". "Modified Date : " . $row["date"]."<br>" ;
 }
 }
 
-
-                  
 mysqli_close($conn);
-?> 
+?>
 </div>
 <br><br>
 
 <form action="editsave.php" method="POST" onsubmit="return confirm('Do you really want to submit the form?');">
-<label >New status of the project</label>
-<select  name="status">
-<option value="">Select the option</option>    
-<option value="option1">option1</option>
-<option value="option2">option2</option>
-<option value="option3">option3</option>
-<option value="option4">option4</option>
-<option value="option5">option5</option>
-<option value="option6">option6</option>
-<option value="option7">option7</option>
-<option value="option8">option8</option>
-<option value="option9">option9</option>
-<option value="option10">option10</option>
+<label >Status of Examiner 1</label>
+<select  name="status1">
 
+<?php
+echo "<option value=".$statuse1.">".$status1[$statuse1][0]."</option>";
+for($row=$statuse1+1;$row<32;$row++)
+  {
+    echo "<option value=".$row.">".$status1[$row][0]."</option>";
+  }
+?>
+</select>
 <br>
 <label >Modified Date</label>
-<input type="date"  name="date" placeholder="Date" required="true">
+<input type="date"  name="date" value= "<?php echo $date1 ?> " placeholder="Date" required="true">
+<br>
+<label >Status of Examiner 2</label>
+<select  name="status2">
 
-<input type="hidden" name="rollno" value=<?php echo $roll ?> >
+<?php
+$statuse2= 1;
+echo "<option value=\"\">".$status2[$statuse2][0]."</option>";
+for($row=$statuse2;$row<32;$row++)
+  {
+    echo "<option value=".$row.">".$status2[$row][0]."</option>";
+  }
+?>
+</select>
+<br>
+<label >Modified Date</label>
+<input type="date"  name="date"  value= "<?php echo $date2 ?> "  placeholder="Date" required="true">
+
+<input type="hidden" name="rollno" value="<?php echo $roll ?> ">
 <input type="submit" value="Save">
 </form>
 
@@ -176,15 +243,15 @@ mysqli_close($conn);
     color: white;
     text-align: center;"
   <center>
-  
+
   Logged in as :
     <?php
      echo $_SESSION['username'];
      ?>
-  
+
   </center>
 </footer>
-   
+
 </body>
 </html>
 
