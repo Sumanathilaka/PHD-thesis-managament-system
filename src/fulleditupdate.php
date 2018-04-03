62e7919df919b72f37d4104e599f4cf6f8f2ebbe
Sumanathilaka <37451505+Sumanathilaka@users.noreply.github.com>
2018-04-03 11:32:29 +0530

    Update fulleditupdate.php

--- src/fulleditupdate.php
+++ src/fulleditupdate.php
@@ -24,7 +24,7 @@ if ($conn->connect_error) {
 
       $name=mysqli_real_escape_string($conn, $_POST['name']);
 	   $rollno=mysqli_real_escape_string($conn, $_POST['rollno']);
-      $email=mysqli_real_escape_string($conn, $_POST['emailid']);
+
       $department=mysqli_real_escape_string($conn, $_POST['department']);
       $guide=mysqli_real_escape_string($conn, $_POST['guidance']);
       $guide_emailid=mysqli_real_escape_string($conn, $_POST['guide_emailid']);
@@ -43,7 +43,6 @@ $sql2 = "UPDATE project SET topic='$topic' WHERE rollno='$roll' ";
 	$sql25 = "UPDATE project SET thesisdate='$tdate' WHERE rollno='$roll' ";
 $sql5 = "UPDATE mtechstudent SET name='$name' WHERE rollno='$roll' ";
 $sql6 = "UPDATE mtechstudent SET rollno='$rollno' WHERE rollno='$roll' ";
-$sql7 = "UPDATE mtechstudent SET email='$email' WHERE rollno='$roll' ";
 $sql8 = "UPDATE mtechstudent SET department='$department' WHERE rollno='$roll' ";
 $sql9 = "UPDATE mtechstudent SET guide='$guide' WHERE rollno='$roll' ";
 $sql10 = "UPDATE mtechstudent SET guidemail='$guide_emailid' WHERE rollno='$roll' ";
@@ -55,7 +54,6 @@ $result= mysqli_query($conn, $sql);
 $result1= mysqli_query($conn, $sql2);
 $result5= mysqli_query($conn, $sql5);
 $result6= mysqli_query($conn, $sql6);
-$result7= mysqli_query($conn, $sql7);
 $result8= mysqli_query($conn, $sql8);
 $result9= mysqli_query($conn, $sql9);
 $result9= mysqli_query($conn, $sql19);	
