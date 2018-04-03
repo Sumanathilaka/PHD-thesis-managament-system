982e9514e44202deac0928cba717f76dd2f89842
mbasilak <37478378+mbasilak@users.noreply.github.com>
2018-04-03 11:37:25 +0530

    Update Notification.php

--- src/Notification.php
+++ src/Notification.php
@@ -32,8 +32,8 @@ $status1 = array
     array("ThesisE1Rem2",7),
     array("ThesisE1Rem3",7),
     array("ThesisE1Rem4",7),
-    array("E1ApprovedThesis",7),
-    array("E1ApprovedHonInitiated",365)
+    array("E1ThesisReviewRecieved",7),
+    array("E1ReviewRecdHonInitiated",365)
    );
    $status2 = array
      (
@@ -67,8 +67,8 @@ $status1 = array
        array("ThesisE2Rem2",7),
        array("ThesisE2Rem3",7),
        array("ThesisE2Rem4",7),
-       array("E2ApprovedThesis",7),
-       array("E2ApprovedHonInitiated",365)
+    array("E2ThesisReviewRecieved",7),
+    array("E2ReviewRecdHonInitiated",365)
       );
 ?>
 
